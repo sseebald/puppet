4cc0e7351bf136607b655ea2bd2ee22489f16a77
Spencer Seebald <sseebald@gmail.com>
2013-07-22 04:31:24 +0000

    Updated

--- setup.sh
+++ setup.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
-    if [ "$(which nginx)" == "" ] ; then
-	if grep nginx /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-	    sudo apt-get install nginx
+if [ "$(which nginx)" == "" ] ; then
+    if grep nginx /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+	sudo apt-get install nginx
+    else
+	sudo add-apt-repository -y ppa:nginx/stable
+	if grep nginx /etc/apt/sources.list.d/*; then
+	    sudo apt-get update
+	    sudo apt-get install -y nginx 
 	else
-	    sudo add-apt-repository -y ppa:nginx/stable
-	    if grep nginx /etc/apt/sources.list.d/*; then
-		sudo apt-get update
-		sudo apt-get install -y nginx 
-	    else
-		echo "PPA failed to be added"
-	    fi
+	    echo "PPA failed to be added"
 	fi
-    else
-        echo "Nginx is installed"
     fi
+else
+    echo "Nginx is installed"
+fi
 
-    if [ "$(which git)" == "" ] ; then
-	sudo apt-get install -y git-core
-    fi
-	
-    DIRECTORY=$(pwd) 
-    DIRECTORY+='/exercise-webpage'
+if [ "$(which git)" == "" ] ; then
+    sudo apt-get install -y git-core
+fi
 
-    if [ -d "$DIRECTORY" ]; then
-	echo "Already downloaded"
-    else
-	git clone https://github.com/puppetlabs/exercise-webpage
-    fi
+DIRECTORY=$(pwd) 
+DIRECTORY+='/exercise-webpage'
 
-    mkdir $HOME/nginx-data
-    mkdir $HOME/nginx-images
-    cp exercise-webpage/index.html $HOME/nginx-data/
+if [ -d "$DIRECTORY" ]; then
+    echo "Already downloaded"
+else
+    git clone https://github.com/puppetlabs/exercise-webpage
+fi
 
-    cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.old
-    
+mkdir $HOME/nginx-data
+mkdir $HOME/nginx-images
+cp exercise-webpage/index.html $HOME/nginx-data/
 
+cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.old
+
+echo $"server {
+      listen 8080;
+        location / {
+                root /data/www;
+        }
+        location /images/ {
+                root /data;
+        }
+        }" >> /etc/nginx/sites-enabled/default
