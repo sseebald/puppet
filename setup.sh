f207ebcb9e43abe77e4186490b53af8eac6add20
Spencer Seebald <sseebald@gmail.com>
2013-07-30 06:54:46 +0000

    Final polish - Removed OS awareness for RHEL, sticking with Debian for now

--- setup.sh
+++ setup.sh
@@ -13,7 +13,11 @@ NGINX_Dir="$(pwd)/nginx"
 # Create our web directories for our site
     if [ -d $NGINX_Dir ]; then
         read -p "Directory $NGINX_Dir already exists - Please specify another directory: " NGINX_Dir
-        NGINX_Dir="$(pwd)/$NGINX_Dir"
+        if $NGINX_Dir != ""; then
+	    NGINX_Dir="$(pwd)/$NGINX_Dir//\//"
+	else
+	    NGINX_Dir="$(pwd)/$NGINX_Dir//\//"
+	fi
         mkdir $NGINX_Dir
     else
         mkdir $NGINX_Dir
@@ -46,16 +50,12 @@ is_Ubuntu=0
 is_RHEL=0
 
 # Loop to determine OS type - To add more supported OS's, add new OS to for line and add logic for it
-for STRING in 'Ubuntu' 'Red Hat' 'CentOS' 'Fedora' 'Debian'
+for STRING in 'Ubuntu' 'Debian'
 do
     if grep -q "$STRING" /etc/*-release; then
         if [ "$STRING" == "Ubuntu" ] || [ "$STRING" == "Debian" ]; then
             is_Ubuntu=1
             echo "Ubuntu/Debian install detected, beginning installation" >> $NGINX_Dir/logs/log.txt
-            break
-        elif [ "$STRING" == "Fedora" ] || [ "$STRING" == "Red Hat" ] || [ "$STRING" == "CentOS" ]; then
-            is_RHEL=1
-            echo "Red Hat/Fedora/CentOS install detected, beginning installation" >> $NGINX_Dir/logs/log.txt
             break
         else
             echo "Unsupported OS detected. Program exitting" >> $NGINX_Dir/logs/log.txt
@@ -82,7 +82,7 @@ if [ "$is_Ubuntu" -eq 1 ]; then
             else
 		RELEASESERVER="$(lsb_release -r)"
                 RELEASESERVER=${RELEASESERVER:(-5):2}
-		if [ "$RELEASESERVER" -ge 10 ] &&  [ "$RELEASESERVER" -le 11 ] ; then
+		if [ "$RELEASESERVER" -ge 11 ]; then
 		    sudo add-apt-repository -y ppa:nginx/stable
 		    echo "Adding nginx/stable repository ... Success" >> $NGINX_Dir/logs/log.tx
 		else
@@ -103,9 +103,6 @@ if [ "$is_Ubuntu" -eq 1 ]; then
 		if [ "$STATUS" -eq 1 ] && [ $X -le 2]; then
 		    # Try to install again, just in case.
 		    STATUS="0"
-		else
-		    echo "Install failed!"
-		    exit
 		fi
 	    fi
 	fi
@@ -154,7 +151,7 @@ if [ "$is_Ubuntu" -eq 1 ]; then
     # TO-DO: Add a check on the defualt config file and make sure no other sites are configured on port 8080. 
     # Also, run a netstat to see if any services are hosted on 8080 already. Pass this to the user if there is.  
 
-    if grep 8080 /etc/nginx/sites-enabled/default; then
+    if [ grep "listen 8080" /etc/nginx/sites-enabled/default ] || [ netstat -tulpn | grep 8080 ]; then
 	echo "There is already a site configured to use port 8080. Reconfigure this site and re-run the installer to save yourself from the wrath of cthulu"
     else
 	echo $"server {
@@ -168,103 +165,11 @@ if [ "$is_Ubuntu" -eq 1 ]; then
         }" >> /etc/nginx/sites-enabled/default
     fi
     # Once we've add our site configuration, reload the nginx configuration file, and restart the service 
-    
-    sudo service nginx stop
-    sudo service nginx start
+    if hash nginx 2>/dev/null; then
+	sudo service nginx stop
+	sudo service nginx start
+	echo "All components successfully installed. Index.html is currently being hosted over port 8080"
 
-    echo "All components successfully installed. Index.html is currently being hosted over port 8080"
-fi
-
-if [ $is_RHEL -eq 1 ]; then
-    STATUS=0
-    
-    while [ $STATUS -eq 0 ]
-    do
-	if hash nginx 2>/dev/null; then
-            echo "NGINX already installed."
-            STATUS="1"
-	else
-            if grep -q nginx /etc/yum.repos.d/*; then
-		sudo yum check-update
-		sudo yum install -y nginx
-		STATUS=$?
-            else
-		RELEASESERVER="$(lsb_release -r)"
-		RELEASESERVER=${RELEASESERVER:(-3):1}
-		
-		echo $"[nginx]
-                           name=nginx repo
-                           baseurl=http://nginx.org/packages/rhel/$RELEASESERVER/\$basearch/
-                           gpgcheck=0
-                           enabled=1" >> /$HOME/nginx.repo
-		
-		sudo cp -r nginx.repo /etc/yum.repos.d/
-		sudo yum check-update
-		sudo yum install -y nginx
-		STATUS=$?
-            fi
-	    
-	    if hash git 2>/dev/null; then
-		sudo yum install -y git-core
-		STATUS=$?
-		echo "Installing git-core ... Success" >> $NGINX_Dir/logs/log.txt
-	fi
-	    
-            if [ $STATUS -eq 1 ] && [ $X -le 2]; then
-		# Try to install again, just in case.
-		STATUS=0
-            else
-		echo "Install failed!"
-            exit
-            fi
-	fi
-    done
-    
-    # Set our current working directory
-    DIRECTORY="$(pwd)/exercise-webpage"
-    
-    # Check to make sure the git directory and file doesnt already exist. If they do not, pull down the index.html
-    if [ -d "$DIRECTORY" ]; then
-    echo "Already downloaded" >> $NGINX_Dir/logs/log.txt
     else
-	git clone https://github.com/puppetlabs/exercise-webpage
-	echo "Creating directory $DIRECTORY ... Success" >> $NGINX_Dir/logs/log.txt
-    fi
-    
-    # Copy over index.html from the Puppet Git repo to our personal nginx web dir
-    cp exercise-webpage/index.html $NGINX_Dir/nginx-data/
-    echo "Copy puppet index.html from git to our data location for nginx" >> $NGINX_Dir/logs/log.txt
-    
-    # Update permissions on the nginx default config file so we can update it without sudo permissions
-    sudo chown $USER:$USER /etc/nginx/sites-enabled/default
-    echo "Updating permissions on /etc/nginx/conf.d/default.conf to $USER:$USER ... Success" >> $NGINX_Dir/logs/\
-log.txt
-    
-    # To avoid causing any irrepairable damage to an already existing configuration (and because it's best
-    # practice), back up the current default configuration
-    cp /etc/nginx/conf.d/default.conf $NGINX_Dir/nginx-data/default.conf.old
-    echo "Default nginx server configuration backed up successfully to $NGINX_Dir/nginx-data/default.conf.old" >> $NGINX_Dir/lo\
-gs/log.txt
-    
-    # Append our own server group to set the port and data directories to the end of the file
-    
-# TO-DO: Add a check on the defualt config file and make sure no other sites are configured on port 8080.
-    # Also, run a netstat to see if any services are hosted on 8080 already. Pass this to the user if there is.
-    
-    echo $"server {
-      listen 8080;
-        location / {
-                root $NGINX_Dir/nginx-data/;
-        }
-        location /images/ {
-                root $NGINX_Dir/nginx-images/;
-        }
-        }" >> /etc/nginx/sites-enabled/default
-
-    # Once we've add our site configuration, reload the nginx configuration file, and restart the service 
-    
-    sudo service nginx stop
-    sudo service nginx start
-    
-    echo "All components successfully installed. Index.html is currently being hosted over port 8080"
+	echo "Program not installed, check the logs for clues"
 fi
